2e5da2227dee49da44b52182a0934a88be915395
Jared Leach <leach.jared@gmail.com>
2019-12-12 17:29:46 -0800

    EOD

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "auctionhouse"
+import (
+	"auctionauth"
+	"auctionhouse"
+	"fmt"
+)
 
 func main() {
-	auctionhouse.GetRealmAddress("r")
+	token, check := auctionauth.GetNewToken()
+	if check {
+		fmt.Println("error in GetNewToken()")
+	}
+	address, check := auctionhouse.GetRealmAddress("us", "tichondrius", token.Token)
+	if check {
+		fmt.Println("error in auctionhouse.GetRealmAddress()")
+	}
+	fmt.Println(address)
+	out, check := auctionhouse.CallRealmAPI(address)
+	if !check {
+		fmt.Println(out)
+	}
+	fmt.Println(auctionhouse.GetRealms())
 }
