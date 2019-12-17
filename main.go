731d9e2442c7cd0d8d4231c9a86c89a1058aa2fc
Jared Leach <leach.jared@gmail.com>
2019-12-16 17:34:55 -0800

    EOD

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	"auctionauth"
 	"auctionhouse"
 	"fmt"
 )
 
 func main() {
-	token, check := auctionauth.GetNewToken()
-	if check {
-		fmt.Println("error in GetNewToken()")
-	}
-	address, check := auctionhouse.GetRealmAddress("us", "tichondrius", token.Token)
-	if check {
-		fmt.Println("error in auctionhouse.GetRealmAddress()")
-	}
-	fmt.Println(address)
-	out, check := auctionhouse.CallRealmAPI(address)
+	// token, check := auctionauth.GetNewToken()
+	// if check {
+	// 	fmt.Println("error in GetNewToken()")
+	// }
+	// address, check := auctionhouse.GetRealmAddress("us", "tichondrius", token.Token)
+	// if check {
+	// 	fmt.Println("error in auctionhouse.GetRealmAddress()")
+	// }
+	// fmt.Println(address)
+	// out, check := auctionhouse.CallRealmAPI(address)
+	// if !check {
+	// 	fmt.Println(out)
+	// }
+	// fmt.Println(auctionhouse.GetRealms())
+	d, check := auctionhouse.NewDaemon("us", "en_US")
 	if !check {
-		fmt.Println(out)
+		fmt.Println(d.Token.Token)
 	}
-	fmt.Println(auctionhouse.GetRealms())
+	out, check := d.GetRealms()
+	if !check {
+		fmt.Println("Bad thangs popping")
+	}
+	fmt.Println(out)
 }
